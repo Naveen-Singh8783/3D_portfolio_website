a8b0680f0c634335a9691d8fcfdd5e821843f296
Naveen Singh <naveensingh4237@gmail.com>
2025-01-20 23:30:02 +0000

    fixing error

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,6 +1,8 @@
 import {Suspense, useEffect, useState} from 'react';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import { extend } from '@react-three/fiber'
+extend({ OrbitControls })
 
 import CanvasLoader from "../Loader";
 
