78aa85d942032ad64f9ec1af8ec07d1a12d223ea
Naveen Singh <naveensingh4237@gmail.com>
2025-02-26 15:56:46 +0000

    adding HOC

---
+++ src/hoc/sectionWrapper.jsx
@@ -0,0 +1,14 @@
+import { motion } from 'framer-motion';
+
+import { styles } from '../styles';
+import { staggerContainer } from '../utils/motion';
+
+const SectionWrapper = (Component, idName) =>{
+    return (
+        <div>
+            SectionWrapper
+        </div>
+    )
+}
+
+export default SectionWrapper;
